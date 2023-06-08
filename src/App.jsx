4c3ebd9827ae7103a81148746d8b9aa3fed151e2
hongjpECG <hongjp@ecgcorp.co.kr>
2023-06-08 16:36:19 +0900

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  // 여기에서 store에 접근하여 counter의 값을 가져오려면,
+  // useSelector를 사용
+  const data = useSelector((state) => state);
+  console.log("data", data);
+
   return <div>Redux!</div>;
 };
 
--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,16 +1,16 @@
 // 초기 상태값
 const initialState = {
-  number: 0,
+  userId: 123,
 };
 
 // 리듀서: state에 변화를 일으키는 함수
 // state를 action의 type에 따라 변경하는 함수
 // 인자값: state, action
-const counter = (state = initialState, action) => {
+const users = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
   }
 };
 
-export default counter;
+export default users;
