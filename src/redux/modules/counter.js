fa4f5b3100402c6f2192ae35716ad274d5928f7a
hongjpECG <hongjp@ecgcorp.co.kr>
2023-06-08 16:25:20 +0900

    redux

---
+++ src/redux/modules/counter.js
@@ -0,0 +1,15 @@
+// 초기 상태값
+const initialState = {
+  number: 0,
+};
+
+// 리듀서: 함수
+// 인자값: state, action
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default counter;
